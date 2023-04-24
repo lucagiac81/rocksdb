e883a2f839cf40961943de65fa0832fbe20fbed2
Mohamed Issa <mohamed.issa@intel.com>
2023-04-24 14:04:09 -0700

    Add options to generate db_bench data from user provided corpus file

--- cache/tiered_secondary_cache_test.cc
+++ cache/tiered_secondary_cache_test.cc
@@ -263,7 +263,7 @@ TEST_F(DBTieredSecondaryCacheTest, BasicTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
@@ -372,7 +372,7 @@ TEST_F(DBTieredSecondaryCacheTest, BasicMultiGetTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
@@ -514,7 +514,7 @@ TEST_F(DBTieredSecondaryCacheTest, WaitAllTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
@@ -615,7 +615,7 @@ TEST_F(DBTieredSecondaryCacheTest, ReadyBeforeWaitAllTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
@@ -725,7 +725,7 @@ TEST_F(DBTieredSecondaryCacheTest, IterateTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
@@ -795,7 +795,7 @@ TEST_P(DBTieredAdmPolicyTest, CompressedOnlyTest) {
   const int N = 256;
   for (int i = 0; i < N; i++) {
     std::string p_v;
-    test::CompressibleString(&rnd, 0.5, 1007, &p_v);
+    test::CompressibleString(&rnd, 0.5, 1007, &p_v, "");
     ASSERT_OK(Put(Key(i), p_v));
   }
 
